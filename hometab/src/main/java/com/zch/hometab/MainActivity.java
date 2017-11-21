97de665cf9dbd7c01f02daa580e84d2c5117c2ed
zch <wb-zch320467@alibaba-inc.com>
2017-11-21 17:44:28 +0800

    add baselib、video module and so on .

---
+++ hometab/src/main/java/com/zch/hometab/MainActivity.java
@@ -0,0 +1,24 @@
+package com.zch.hometab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R2.id.main_tv_content)
+    TextView tvContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        tvContent.setText("Welcome to mobile manager .");
+    }
+}
